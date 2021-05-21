564aa251eafe48e7d5a967287e745cadc2ba1e46
rcbruijn1 <rcbruijn1@avans.nl>
2021-05-21 13:09:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,61 +15,32 @@ var journey_routes_v1 = require('./api/journey.routes.v1');
 
 var config = require('./config/env');
 
-// Met module.exports kunnen we variabelen beschikbaar maken voor andere bestanden.
 module.exports = {};
 
-// bodyParser zorgt dat we de body uit een request kunnen gebruiken,
-// hierin zit de inhoud van een POST request.
 app.use(bodyParser.urlencoded({
     'extended': 'true'
-})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
+}));
+app.use(bodyParser.json());
 app.use(bodyParser.json({
     type: 'application/vnd.api+json'
-})); // parse application/vnd.api+json as json
+}));
 
 //PASSPORT middleware
+require('./config/passport')(passport);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./config/passport')(passport);
-
-
-// CORS headers
 app.use(cors());
 
-// app.use(function (req, res, next) {
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN || 'http://localhost:4200');
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     // Pass to next layer of middleware
-//     next();
-// });
-
-// app.use(express.static(path.join(_dirname, 'public')));
-
-// Installeer de routers
 app.use('/api/v1', city_routes_v1);
 app.use('/api/v1', city_location_routes_v1);
 app.use('/api/v1/user', users_routes_v1);
 app.use('/api/v1', journey_routes_v1);
 
-// configureer de app
 app.set('port', (process.env.PORT || config.env.webPort));
-// app.set('env', (process.env.ENV || 'development'))
 
-
-// Errorhandler voor express-jwt errors
-// Wordt uitgevoerd wanneer err != null; anders door naar next().
 app.use(function (err, req, res, next) {
-    // console.dir(err);
     var error = {
         message: err.message,
         code: err.code,
@@ -79,26 +50,16 @@ app.use(function (err, req, res, next) {
     res.status(401).send(error);
 });
 
-// Fallback - als geen enkele andere route slaagt wordt deze uitgevoerd.
-// app.use('*', function (req, res) {
-//     res.status(400);
-//     res.json({
-//         'error': 'Deze URL is niet beschikbaar.'
-//     });
-// });
-
-// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
   });
 
-// Installatie klaar; start de server.
 app.listen(config.env.webPort, function () {
     console.log('De server luistert op port ' + app.get('port'));
     console.log('Zie bijvoorbeeld http://localhost:3000/api/v1/cities');
 });
 
-// Voor testen met mocha/chai moeten we de app exporteren.
+
 module.exports = app;
